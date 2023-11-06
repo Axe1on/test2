75ca9dfe42698771b60ef07ee1c85bb12303941a
Edgar <Meccu9i@gmail.com>
2023-11-06 23:05:38 +0300

    First commit

---
+++ src/Mains.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Mains {
+
+    public static void main(String[] args) {
+
+        String text = "Regular Expressions or Regex is an API for defining String patterns that can be used for searching, manipulating and editing a text. It is widely used to define a constraint on strings such as a password. Regular Expressions are provided under java.util.regex package.";
+        Pattern pattern = Pattern.compile("\ba[a-z]*?a\b");
+        Matcher matcher = pattern.matcher(text);
+
+            System.out.println("ответ "+ matcher);
+    }
+}
